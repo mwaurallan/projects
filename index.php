1dee8a5dd7227513bc88297dbd8a338a0d4248ae
mwaurallan <allanmwaura2013@gmail.com>
2017-07-06 02:35:41 +0300

    change index

--- index.php
+++ index.php
@@ -127,7 +127,8 @@
                 </div>
                 <div class="top-right links">
                     <a href="home.php">Members</a>
-                    <a href="savings.php">Savings</a>
+                    <a href="banks.php">Banks</a>
+                    <a href="Deposits.php">Deposits</a>
                     <ul class="nav navbar-nav navbar-right dropdown">
                         <button class="dropbtn">Reports</button>
                         <div class="dropdown-content">
